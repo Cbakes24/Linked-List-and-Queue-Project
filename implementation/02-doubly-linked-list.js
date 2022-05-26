3335d28e5a0007c2914b161c43605b49e0b2e5ac
Cory Baker <corybaker24@gmail.com>
2022-05-25 20:15:50 -0700

    removeFromHEad doubly

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -51,7 +51,18 @@ class DoublyLinkedList {
   }
 
   removeFromHead() {
-    // Remove node at head
+    if(this.length === 0) return undefined;
+    var oldHead = this.head;
+    if(this.length === 1){
+        this.head = null;
+        this.tail = null;
+    }else{
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+    }
+    this.length--;
+    return oldHead.value;
     // Write your hypothesis on the time complexity of this method here
   }
 
